c1c7a13a4c3d0e60c1a6ec1432802aeee8ed70a8
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-06 03:02:04 +0530

    refactor: update NavFavorites layout and add dark mode styles

--- src/components/sidebar/nav-favorites.tsx
+++ src/components/sidebar/nav-favorites.tsx
@@ -27,19 +27,19 @@ interface NavFavoritesProps {
 
 const NavFavorites: React.FunctionComponent<NavFavoritesProps> = () => {
   return (
-    <SidebarGroup className="px-5 group-data-[collapsible=icon]:hidden">
+    <SidebarGroup className="px-4 py-0 pb-3 group-data-[collapsible=icon]:hidden">
       <SidebarMenu>
         <Collapsible asChild className="group/collapsible">
           <Tabs
             defaultValue="favorites"
             className="flex max-w-54 flex-col items-start gap-1 self-stretch"
           >
-            <TabsList className="flex flex-wrap content-center items-center gap-2 self-stretch text-center text-sm leading-5 font-normal text-black/40">
+            <TabsList className="flex flex-wrap content-center items-center gap-2 self-stretch text-center text-sm leading-5 font-normal">
               {FAVORITES_DATA.map((tab) => (
                 <TabsTrigger
                   key={tab.key}
                   value={tab.key}
-                  className="flex flex-wrap content-center items-center gap-2 self-stretch px-2 data-[state=active]:text-black/40 data-[state=inactive]:text-black/20"
+                  className="flex flex-wrap content-center items-center gap-2 self-stretch px-2 py-1 data-[state=active]:text-black/40 data-[state=inactive]:text-black/20 dark:data-[state=active]:text-gray-500 dark:data-[state=inactive]:text-gray-500"
                 >
                   {tab.title}
                 </TabsTrigger>
@@ -47,12 +47,12 @@ const NavFavorites: React.FunctionComponent<NavFavoritesProps> = () => {
             </TabsList>
             {FAVORITES_DATA.map((tab) => (
               <TabsContent key={tab.key} value={tab.key}>
-                <ul className="flex list-inside list-disc flex-col gap-1 pl-3 text-sm font-normal [&>li::marker]:text-black/20">
+                <ul className="flex list-inside list-disc flex-col gap-1 text-sm font-normal [&>li::marker]:text-black/20 dark:[&>li::marker]:text-gray-500">
                   {tab.data.map((item) => (
-                    <li key={item.id} className="py-1">
+                    <li key={item.id} className="px-3 py-1">
                       <a
                         href={item.url}
-                        className="hover:text-sidebar-accent-foreground transition-colors"
+                        className="hover:text-sidebar-accent-foreground relative -left-1 transition-colors"
                       >
                         {item.title}
                       </a>
