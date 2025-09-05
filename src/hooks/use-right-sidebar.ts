6708e5e1842e4bdac4ba4f5b779301ba0d676e82
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-06 03:06:24 +0530

    feat: create RightSidebarContext and useRightSidebar hook

---
+++ src/hooks/use-right-sidebar.ts
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export type RightSidebarContextProps = {
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
+  toggle: () => void;
+};
+
+const RightSidebarContext = React.createContext<RightSidebarContextProps | null>(null);
+
+export function useRightSidebar() {
+  const context = React.useContext(RightSidebarContext);
+  if (!context) {
+    throw new Error("useRightSidebar must be used within a RightSidebarProvider.");
+  }
+  return context;
+}
+
+export { RightSidebarContext };
