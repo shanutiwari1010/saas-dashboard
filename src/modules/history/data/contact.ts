656567862da01c47182d9b28fe54ba3ce64c34b0
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 00:54:56 +0530

    feat: add contacts mock data

---
+++ src/modules/history/data/contact.ts
@@ -0,0 +1,46 @@
+import type { Contact } from "../interfaces/contact";
+
+export const contacts: Contact[] = [
+  {
+    id: 1,
+    name: "Natali Craig",
+    email: "natali.craig@gmail.com",
+    avatar: "/avatars/natali-craig.svg",
+    status: "online",
+  },
+  {
+    id: 2,
+    name: "Drew Cano",
+    email: "drew.cano@gmail.com",
+    avatar: "/avatars/drew-cano.svg",
+    status: "away",
+  },
+  {
+    id: 3,
+    name: "Orlando Diggs",
+    email: "orlando.diggs@gmail.com",
+    avatar: "/avatars/orlando-diggs.svg",
+    status: "offline",
+  },
+  {
+    id: 4,
+    name: "Andi Lane",
+    email: "andi.lane@gmail.com",
+    avatar: "/avatars/andi-lane.svg",
+    status: "online",
+  },
+  {
+    id: 5,
+    name: "Kate Morrison",
+    email: "kate.morrison@gmail.com",
+    avatar: "/avatars/kate-morrison.svg",
+    status: "online",
+  },
+  {
+    id: 6,
+    name: "Koray Okumus",
+    email: "koray.okumus@gmail.com",
+    avatar: "/avatars/koray-okumus.svg",
+    status: "away",
+  },
+];
