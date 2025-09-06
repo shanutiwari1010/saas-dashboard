c2cf94bad8ef90290d636cce83f240fdc3c06d49
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 03:22:49 +0530

    refactor: reorder imports and export SidebarHeader as named export

--- src/components/sidebar/sidebar-header.tsx
+++ src/components/sidebar/sidebar-header.tsx
@@ -1,11 +1,12 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { RightSidebarTrigger } from "@/components/ui/right-sidebar-trigger";
-import { Button } from "@/components/ui/button";
 import { ClockCounterClockwise, Star, Bell, Sidebar } from "phosphor-react";
-import { ModeToggle } from "@/components/ui/mode-toggle";
-import { Breadcrumbs } from "../breadcrumbs";
 
-const SidebarHeader = () => {
+import { Button } from "@/components/ui/button";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { RightSidebarTrigger } from "@/components/ui/right-sidebar-trigger";
+
+export const SidebarHeader = () => {
   return (
     <header className="flex h-[68px] shrink-0 items-center justify-between gap-2 border-b px-7 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2">
@@ -30,5 +31,3 @@ const SidebarHeader = () => {
     </header>
   );
 };
-
-export default SidebarHeader;
