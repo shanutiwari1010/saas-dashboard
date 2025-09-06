7b428835cd9392426219813c0e00564e69349b46
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 03:43:14 +0530

    chore: add top selling products data

---
+++ src/modules/dashboard/data/products.ts
@@ -0,0 +1,41 @@
+import type { TopSellingProduct } from "@/modules/dashboard/types/product";
+
+export const TOP_SELLING_PRODUCTS: TopSellingProduct[] = [
+  {
+    id: "1",
+    name: "ASOS Ridley High Waist",
+    price: 79.49,
+    quantity: 82,
+    amount: 6518.18,
+  },
+  {
+    id: "2",
+    name: "Marco Lightweight Shirt",
+    price: 128.5,
+    quantity: 37,
+    amount: 4754.5,
+  },
+  {
+    id: "3",
+    name: "Half Sleeve Shirt",
+    price: 39.99,
+    quantity: 64,
+    amount: 2559.36,
+  },
+  {
+    id: "4",
+    name: "Lightweight Jacket",
+    price: 20.0,
+    quantity: 184,
+    amount: 3680.0,
+  },
+  {
+    id: "5",
+    name: "Marco Shoes",
+    price: 79.49,
+    quantity: 64,
+    amount: 1965.81,
+  },
+];
+
+export default TOP_SELLING_PRODUCTS;
