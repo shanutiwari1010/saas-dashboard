6fed970f72cde756728a7a420b8a1b1efd0d82f1
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 00:50:53 +0530

    feat: add MainLayout with left & right sidebars, header, and outlet

---
+++ src/layouts/main-layout.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+import { SidebarLeftPanel } from "@/components/sidebar/sidebar-left-panel";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { RightSidebarProvider } from "@/providers/right-sidebar-provider";
+import SidebarHeader from "@/components/sidebar/sidebar-header";
+import { SidebarRightPanel } from "@/components/sidebar/sidebar-right-panel";
+
+export function MainLayout() {
+  return (
+    <SidebarProvider>
+      <RightSidebarProvider>
+        <div className="flex h-screen w-full">
+          <SidebarLeftPanel />
+          <SidebarInset>
+            <SidebarHeader />
+            <Outlet />
+          </SidebarInset>
+          <SidebarRightPanel />
+        </div>
+      </RightSidebarProvider>
+    </SidebarProvider>
+  );
+}
