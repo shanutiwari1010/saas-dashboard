2ddbb1bd77ef0fd6d34051b28ee663e8af8cbd72
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 03:37:46 +0530

    chore: update TopSellingProducts table

--- src/modules/dashboard/components/top-selling-products.tsx
+++ src/modules/dashboard/components/top-selling-products.tsx
@@ -1,15 +1,13 @@
 import { Card } from "@/components/ui/card";
-import topSellingProducts from "../data/top-selling-products";
-
-// Revenue data for different locations
+import topSellingProducts from "@/modules/dashboard/data/products";
 
 export default function TopSellingProducts() {
   return (
-    <Card className="w-3xl border-none bg-gray-50 p-6">
-      <h2 className="heading mb-6">Top Selling Products</h2>
+    <Card className="space-y-1 border-none bg-gray-50 p-6 shadow-none">
+      <h2 className="heading">Top Selling Products</h2>
       <div className="overflow-x-auto">
         <table className="w-full">
-          <thead>
+          <thead className="text-xs leading-4 font-normal text-black/40">
             <tr className="border-b border-black/10 bg-gray-50">
               <th className="py-3 text-left font-light text-black/40">Name</th>
               <th className="py-3 text-left font-light text-black/40">Price</th>
@@ -23,13 +21,12 @@ export default function TopSellingProducts() {
           </thead>
           <tbody>
             {topSellingProducts.map((product) => (
-              <tr key={product.name} className="border-b border-gray-100">
-                <td className="py-4 font-light ">
-                  {product.name}
-                </td>
-                <td className="py-4 font-light">
-                  ${product.price.toFixed(2)}
-                </td>
+              <tr
+                key={product.name}
+                className="text-xs leading-4 font-normal text-black"
+              >
+                <td className="py-4 font-light">{product.name}</td>
+                <td className="py-4 font-light">${product.price.toFixed(2)}</td>
                 <td className="py-4 font-light">{product.quantity}</td>
                 <td className="py-4 font-light">
                   $
