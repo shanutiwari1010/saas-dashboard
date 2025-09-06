2204bd4310eedfdad961d4bb0db62b7352c74b9e
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 00:44:46 +0530

    refactor(header): replace inline breadcrumb with Breadcrumbs component

--- src/components/sidebar/sidebar-header.tsx
+++ src/components/sidebar/sidebar-header.tsx
@@ -1,38 +1,19 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { RightSidebarTrigger } from "@/components/ui/right-sidebar-trigger";
 import { Button } from "@/components/ui/button";
 import { ClockCounterClockwise, Star, Bell, Sidebar } from "phosphor-react";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Breadcrumbs } from "../breadcrumbs";
 
 const SidebarHeader = () => {
   return (
     <header className="flex h-[68px] shrink-0 items-center justify-between gap-2 border-b px-7 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2">
-        <SidebarTrigger className="" />
+        <SidebarTrigger />
         <Button variant="ghost" size="icon">
           <Star size={20} weight="duotone" className="size-10" />
         </Button>
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">
-                Building Your Application
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+        <Breadcrumbs />
       </div>
       <div className="flex items-center gap-2">
         <ModeToggle />
@@ -40,10 +21,10 @@ const SidebarHeader = () => {
           <ClockCounterClockwise size={20} weight="duotone" />
         </RightSidebarTrigger>
         <RightSidebarTrigger variant="ghost" size="icon">
-          <Sidebar size={20} weight="duotone" />
+          <Bell size={20} weight="duotone" />
         </RightSidebarTrigger>
         <RightSidebarTrigger variant="ghost" size="icon">
-          <Bell size={20} weight="duotone" />
+          <Sidebar size={20} weight="duotone" />
         </RightSidebarTrigger>
       </div>
     </header>
