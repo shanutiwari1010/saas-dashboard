76cf2988ec58936bbf0ed85e5253a85b1c4bf497
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 03:24:02 +0530

    refactor: organize imports and structure panel content

--- src/components/sidebar/sidebar-right-panel.tsx
+++ src/components/sidebar/sidebar-right-panel.tsx
@@ -1,18 +1,15 @@
 import { RightSidebar } from "@/components/ui/right-sidebar";
-
+import { ContactsLists } from "@/modules/history/components/contacts-lists";
 import { Notifications } from "@/modules/notification/components/notification";
 import { TimelineActivities } from "@/modules/history/components/timeline-activities";
-import { ContactsLists } from "@/modules/history/components/contacts-lists";
 
 export function SidebarRightPanel() {
   return (
     <RightSidebar className="bg-background border-l">
-      <div className="flex h-full flex-col">
-       
-          <Notifications />
-          <TimelineActivities />
-          <ContactsLists />
-       
+      <div className="flex h-full flex-col overflow-y-auto">
+        <Notifications />
+        <TimelineActivities />
+        <ContactsLists />
       </div>
     </RightSidebar>
   );
