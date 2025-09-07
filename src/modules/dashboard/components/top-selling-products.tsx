4eeb44820026d50666c6c1478cf88cd9bd547576
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 20:53:45 +0530

    refactor: update TopSellingProducts with constants, dark mode & styling

--- src/modules/dashboard/components/top-selling-products.tsx
+++ src/modules/dashboard/components/top-selling-products.tsx
@@ -1,34 +1,32 @@
 import { Card } from "@/components/ui/card";
-import topSellingProducts from "@/modules/dashboard/data/products";
+import { TOP_SELLING_PRODUCTS } from "@/modules/dashboard/data/products";
 
-export default function TopSellingProducts() {
+export function TopSellingProducts() {
   return (
-    <Card className="space-y-1 border-none bg-gray-50 p-6 shadow-none">
+    <Card className="bg-dashboard-revenue space-y-1 border-none p-6 shadow-none">
       <h2 className="heading">Top Selling Products</h2>
       <div className="overflow-x-auto">
         <table className="w-full">
-          <thead className="text-xs leading-4 font-normal text-black/40">
-            <tr className="border-b border-black/10 bg-gray-50">
-              <th className="py-3 text-left font-light text-black/40">Name</th>
-              <th className="py-3 text-left font-light text-black/40">Price</th>
-              <th className="py-3 text-left font-light text-black/40">
-                Quantity
-              </th>
-              <th className="py-3 text-left font-light text-black/40">
-                Amount
-              </th>
+          <thead className="text-left text-xs leading-4 text-black/40 dark:text-white/40">
+            <tr className="border-b border-black/10">
+              <th className="py-3 font-normal">Name</th>
+              <th className="py-3 font-normal">Price</th>
+              <th className="py-3 font-normal">Quantity</th>
+              <th className="py-3 font-normal">Amount</th>
             </tr>
           </thead>
           <tbody>
-            {topSellingProducts.map((product) => (
+            {TOP_SELLING_PRODUCTS.map((product) => (
               <tr
                 key={product.name}
-                className="text-xs leading-4 font-normal text-black"
+                className="text-xs leading-4 font-normal text-black dark:text-white"
               >
-                <td className="py-4 font-light">{product.name}</td>
-                <td className="py-4 font-light">${product.price.toFixed(2)}</td>
-                <td className="py-4 font-light">{product.quantity}</td>
-                <td className="py-4 font-light">
+                <td className="py-4 font-normal">{product.name}</td>
+                <td className="py-4 font-normal">
+                  ${product.price.toFixed(2)}
+                </td>
+                <td className="py-4 font-normal">{product.quantity}</td>
+                <td className="py-4 font-normal">
                   $
                   {product.amount.toLocaleString("en-US", {
                     minimumFractionDigits: 2,
