82169dd5aa34889e8ac8bfed89d7ee07891312db
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 21:03:48 +0530

    fix: change cards id

--- src/modules/dashboard/data/metrics.ts
+++ src/modules/dashboard/data/metrics.ts
@@ -1,48 +1,45 @@
 import {
   MetricTrend,
+  MetricCardIds,
   MetricChangeType,
   type MetricCard,
 } from "@/modules/dashboard/types/metrics";
 
 export const DASHBOARD_METRICS: MetricCard[] = [
   {
-    id: "customers",
+    id: MetricCardIds.CUSTOMERS,
     title: "Customers",
     value: "3,781",
     change: 11.01,
     changeType: MetricChangeType.INCREASE,
     trend: MetricTrend.UP,
-    // color: DASHBOARD_CONSTANTS.METRIC_COLORS.CUSTOMERS,
     description: "Total number of active customers",
   },
   {
-    id: "orders",
+    id: MetricCardIds.ORDERS,
     title: "Orders",
     value: "1,219",
     change: 0.03,
     changeType: MetricChangeType.DECREASE,
     trend: MetricTrend.DOWN,
-    // color: DASHBOARD_CONSTANTS.METRIC_COLORS.ORDERS,
     description: "Total number of orders placed",
   },
   {
-    id: "revenue",
+    id: MetricCardIds.REVENUE,
     title: "Revenue",
     value: "$695",
     change: 15.03,
     changeType: MetricChangeType.INCREASE,
     trend: MetricTrend.UP,
-    // color: DASHBOARD_CONSTANTS.METRIC_COLORS.REVENUE,
     description: "Total revenue generated",
   },
   {
-    id: "growth",
+    id: MetricCardIds.GROWTH,
     title: "Growth Rate",
     value: "30.1%",
     change: 6.08,
     changeType: MetricChangeType.INCREASE,
     trend: MetricTrend.UP,
-    // color: DASHBOARD_CONSTANTS.METRIC_COLORS.GROWTH,
     description: "Month-over-month growth rate",
   },
-];
+] as const;
