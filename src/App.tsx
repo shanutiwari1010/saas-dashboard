9a37d65520d39b9d15893e2578f3be5af76d92af
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 23:07:21 +0530

    chore: add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "@/router";
+import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
+      <Toaster />
     </ThemeProvider>
   );
 }
