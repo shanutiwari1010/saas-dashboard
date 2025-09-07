86fcdbcd7dc7109010fe900924e0460a39563a29
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-07 20:43:50 +0530

    ui: disable system theme option

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -35,7 +35,12 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem
+          disabled
+          aria-disabled
+          className="cursor-not-allowed"
+          onClick={() => setTheme("system")}
+        >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
