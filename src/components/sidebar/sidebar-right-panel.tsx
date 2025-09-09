105d5abb4e08e688d3f2b0d05f858efcf58c8220
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-09-09 15:38:28 +0530

    chore: add react memo

--- src/components/sidebar/sidebar-right-panel.tsx
+++ src/components/sidebar/sidebar-right-panel.tsx
@@ -1,11 +1,15 @@
+import React from "react";
 import { RightSidebar } from "@/components/ui/right-sidebar";
 import { ContactsLists } from "@/modules/history/components/contacts-lists";
 import { Notifications } from "@/modules/notification/components/notification";
 import { TimelineActivities } from "@/modules/history/components/timeline-activities";
+import type { Sidebar } from "@/components/ui/sidebar";
 
-export function SidebarRightPanel() {
+export const SidebarRightPanel = React.memo<
+  React.ComponentProps<typeof Sidebar>
+>(({ ...props }) => {
   return (
-    <RightSidebar className="bg-background border-l">
+    <RightSidebar className="bg-background border-l" {...props}>
       <div className="flex h-full flex-col overflow-y-auto">
         <Notifications />
         <TimelineActivities />
@@ -13,4 +17,4 @@ export function SidebarRightPanel() {
       </div>
     </RightSidebar>
   );
-}
+});
